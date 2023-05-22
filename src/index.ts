054d9d7da2d8c2083a15e6b614a394c58c0935fb
web3ten0 <web3ten0@gmail.com>
2023-05-22 23:04:38 +0900

    added /users/add

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { poweredBy } from "hono/powered-by";
 import { drizzle } from "drizzle-orm/d1";
+import { InferModel } from "drizzle-orm";
 import { users } from "./models/users";
 
 const app = new Hono();
@@ -23,6 +24,14 @@ app.get("/posts", (c) => {
 app.get("/users", async (c) => {
   const db = drizzle(c.env.DB);
   const result = await db.select().from(users).all();
+  return c.json(result);
+});
+
+app.post("/users/add", async (c) => {
+  const db = drizzle(c.env.DB);
+  type NewUser = InferModel<typeof users, "insert">;
+  const user = await c.req.json<NewUser>();
+  const result = await db.insert(users).values(user).run();
   return c.json(result);
 });
 
